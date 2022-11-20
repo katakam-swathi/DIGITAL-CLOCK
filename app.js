eb34ddbabef9993aa877449c261808d2a65b22f3
katakam-swathi <118615361+katakam-swathi@users.noreply.github.com>
2022-11-20 12:25:27 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ function updateClock() {
       ampm="PM"
   
     }
-  
+    if (h=12){
+      ampm="pm"
+    }
     h = h < 10 ? "0" + h : h;
     m = m < 10 ? "0" + m : m;
     s = s < 10 ? "0" + s : s;
